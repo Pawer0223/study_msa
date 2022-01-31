4a441873882ac62276c5ac928f7fa9a6885bff7b
taesan <pawer02023@gmail.com>
2022-02-01 04:07:34 +0900

    apigateway filter java config & request, response header checking

--- second-service/src/main/java/com/examplz/secondservice/SecondServiceController.java
+++ second-service/src/main/java/com/examplz/secondservice/SecondServiceController.java
@@ -1,14 +1,23 @@
 package com.examplz.secondservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController()
+@Slf4j
+@RestController
 @RequestMapping("/second-service")
 public class SecondServiceController {
     @GetMapping("/welcome")
     public String index() {
         return "Welcome to the Second Service.";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header) {
+        log.info("header : {}", header);
+        return "Hello World in second Service.";
     }
 }
